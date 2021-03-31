85e765ab92ae36f4c4a5a22a24685f1f71f4f595
JuanCarlosSalazarMuñoz <juan_salazar82182@elpoli.edu.co>
2021-03-31 13:36:48 -0500

    Iniciando proyecto (Clase nodo lista, comenzando métodos de clase árbol

---
+++ src/arboln/ArbolN.java
@@ -0,0 +1,16 @@
+package arboln;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @authors Ormolgud Gonzalez Cardona
+ *          Juan Carlos Salazar Muñoz
+ */
+public class ArbolN {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "A basic JOptionPane message dialog");
+        
+    }
+    
+}
